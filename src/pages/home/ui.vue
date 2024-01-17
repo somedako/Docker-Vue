5dceb196272accd0162d2ebf7d7c91a41a1549ab
Aleksei Trofimov <vp8887@mail.ru>
2024-01-17 21:13:06 +0400

    add component Card

--- src/pages/home/ui.vue
+++ src/pages/home/ui.vue
@@ -1,14 +1,39 @@
 <script setup lang="ts">
+import { reactive } from 'vue'
 import { Header } from '@/widgets/header'
 import { BottomTabNavigator } from '@/widgets/bottom-tab-navigator'
 import { Carousel } from '@/features/carousel'
 import { Footer } from '@/widgets/footer'
 import { Content } from '@/shared/content'
+import { Cards, type CardsProps } from '@/widgets/cards'
+import { Container } from '@/shared/container'
+
+import product1PNG from '@/assets/product-1.png'
+
+const saleCards = reactive<CardsProps>({
+  data: {
+    title: 'Акции',
+    listLinkText: 'Все акции',
+    listLinkHref: '/',
+    items: [
+      {
+        img: product1PNG,
+        name: 'Г/Ц Блинчики с мясом вес, Россия',
+        sale: 50
+      }
+    ]
+  }
+})
 </script>
 
 <template>
   <Header />
   <BottomTabNavigator />
-  <Content> <Carousel /></Content>
+  <Content>
+    <Carousel />
+    <Container :style="{ margin: '80px auto' }">
+      <Cards :data="saleCards.data" />
+    </Container>
+  </Content>
   <Footer />
 </template>
